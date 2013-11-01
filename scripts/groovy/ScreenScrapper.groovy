c88fc6536d528d49e03018c390e07a9a18d0f41d
khaing211 <khaing211@gmail.com>
2013-11-01 15:29:25 -0700

    Add ScreenScapper implemented by the awesome geb (selenium/webdriver abstraction) library =D

---
+++ scripts/groovy/ScreenScrapper.groovy
@@ -0,0 +1,33 @@
+@Grapes([
+	@Grab(group = 'org.seleniumhq.selenium', module = 'selenium-java', version = '2.37.1'),
+	@Grab(group = 'org.gebish', module = 'geb-core', version = '0.9.2')
+])
+
+import geb.Browser
+
+final def cli = new CliBuilder(usage: 'ScreenScapper')
+cli.h( longOpt: 'help', required: false, 'show usage information' )
+cli.u( longOpt: 'url', argName: 'u', required: true, args: 1, 'absolute url path i.e. http://google.com' )
+
+//--------------------------------------------------------------------------
+final def opt = cli.parse(args)
+if (!opt) { return }
+if (opt.h) {
+	cli.usage();
+	return
+}
+
+final def url = opt.u
+
+Browser.drive {
+    go "${url}"
+     
+    $('a').allElements().each {
+		println it.getAttribute('href')
+	}
+	
+	$('img').allElements().each {
+		println it.getAttribute('src')
+	}
+}
+
