6bed35a8a707a9227e02a987e2fd6ce43ab4e222
khaing211 <khaing211@gmail.com>
2013-11-03 19:22:08 -0800

    + Add result check + Add map marker + I didn't know that map static api can already take an address

--- src/main/groovy/ShowMap.groovy
+++ src/main/groovy/ShowMap.groovy
@@ -2,6 +2,11 @@
 	@Grab(group='org.apache.httpcomponents', module='fluent-hc', version='4.3.1')
 ])
 
+/**
+ * ShowMap show a gui display the map for the supplied address
+ * 
+ * TODO: add a keyclick listener for next/back to go through each result
+ */
 
 import org.apache.http.client.fluent.Request
 import org.apache.http.entity.ContentType
@@ -16,12 +21,12 @@ import groovy.swing.SwingBuilder
 import groovy.json.JsonSlurper
 import java.net.URLEncoder
 
-final def cli = new CliBuilder(usage: 'ShowMap')
+final def cli = new CliBuilder(usage: 'ShowMap i.e. groovy ShowMap.groovy -a "1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA"')
 cli.h( longOpt: 'help', required: false, 'show usage information' )
 cli.a( longOpt: 'address', argName: 'a', required: true, args: 1, 'address of the place' )
-cli.l( longOpt: 'length', argName: 'l', required: false, args: 1, 'height of the map' )
-cli.w( longOpt: 'width', argName: 'w', required: false, args: 1, 'width of the map' )
-cli.z( longOpt: 'zoom', argName: 'z', required: false, args: 1, 'zoom level from space' )
+cli.l( longOpt: 'length', argName: 'l', required: false, args: 1, 'height of the map, default 512' )
+cli.w( longOpt: 'width', argName: 'w', required: false, args: 1, 'width of the map, default 512' )
+cli.z( longOpt: 'zoom', argName: 'z', required: false, args: 1, 'zoom level from space, default 15' )
 
 //--------------------------------------------------------------------------
 final def opt = cli.parse(args)
@@ -32,44 +37,46 @@ if (opt.h) {
 }
 
 final def address = URLEncoder.encode(opt.a, 'UTF-8')
-final def height = opt.l ? opt.l as int : 500
-final def width = opt.w ? opt.w as int : 500
-final def zoom = opt.z ? opt.z as int : 10
+final def height = opt.l ? opt.l as int : 512
+final def width = opt.w ? opt.w as int : 512
+final def zoom = opt.z ? opt.z as int : 15
 
 final def slurper = new JsonSlurper()
 
 final def geocodingApi = "http://maps.googleapis.com/maps/api/geocode/json?address=${address}&sensor=false"
 final def geocodingContent = slurper.parseText(Request.Get(geocodingApi).execute().returnContent().asString())
-/*
- * "OK" indicates that no errors occurred; the address was successfully parsed and at least one geocode was returned.
-"ZERO_RESULTS" indicates that the geocode was successful but returned no results. This may occur if the geocode was passed a non-existent address or a latlng in a remote location.
-"OVER_QUERY_LIMIT" indicates that you are over your quota.
-"REQUEST_DENIED" indicates that your request was denied, generally because of lack of a sensor parameter.
-"INVALID_REQUEST" generally indicates that the query (address or latlng) is missing.
-UNKNOWN_ERROR indicates that the request could not be processed due to a server error. The request may succeed if you try again.
- */
 
-println geocodingContent.status
-println geocodingContent.results.size()
-
-final def staticMapApi = "http://maps.googleapis.com/maps/api/staticmap?center=-15.800513,-47.91378&zoom=${zoom}&size=${height}x${width}&sensor=false"
-
-final def staticMapContent = Request.Get(staticMapApi).execute().returnContent()
-final Iterator<?> readers = ImageIO.getImageReadersByMIMEType(staticMapContent.getType().getMimeType())
-final ImageReader reader = (ImageReader) readers.next()
-final ImageInputStream iis = ImageIO.createImageInputStream(staticMapContent.asStream())
-
-reader.setInput(iis, true)
-final ImageReadParam param = reader.getDefaultReadParam()
-final Image image = reader.read(0, param)
-
-new SwingBuilder().edt {
-	frame(title: address, size:[height, width], show: true, 
-		defaultCloseOperation: JFrame.EXIT_ON_CLOSE, resizable: false, 
-		locationRelativeTo: null) {
-		
-		label(icon:imageIcon(image : image))
+if (geocodingContent.status == 'OK') {
+	println "Found ${geocodingContent.results.size()} results and display first result"
+	
+	final def firstResult = geocodingContent.results[0]
+	final def resultAddress = firstResult.formatted_address
+	final def lat = firstResult.geometry.location.lat
+	final def lng = firstResult.geometry.location.lng
+	
+	final def staticMapApi = "http://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=${height}x${width}&sensor=false&markers=size:mid%7Ccolor:blue%7C${lat},${lng}"
+	
+	final def staticMapContent = Request.Get(staticMapApi).execute().returnContent()
+	final Iterator<?> readers = ImageIO.getImageReadersByMIMEType(staticMapContent.getType().getMimeType())
+	final ImageReader reader = (ImageReader) readers.next()
+	final ImageInputStream iis = ImageIO.createImageInputStream(staticMapContent.asStream())
+	
+	reader.setInput(iis, true)
+	final ImageReadParam param = reader.getDefaultReadParam()
+	final Image image = reader.read(0, param)
+	
+	new SwingBuilder().edt {
+		frame(title: address, size:[height, width], show: true,
+			defaultCloseOperation: JFrame.EXIT_ON_CLOSE, resizable: false,
+			locationRelativeTo: null) {
+			
+			label(icon:imageIcon(image : image))
+		}
 	}
+} else {
+	println geocodingContent.status
 }
+
+
 
 
