093177e9ba8823eb3e593048e328decbc1fc4fca
Khai Nguyen <khaing211@gmail.com>
2013-11-05 13:10:41 -0800

    Create SeleniumSetBrowserDimensionExample.groovy
    Set browser dimension before opening using selenium

---
+++ src/main/example/SeleniumSetBrowserDimensionExample.groovy
@@ -0,0 +1,19 @@
+@Grapes([
+	@Grab(group = 'org.seleniumhq.selenium', module = 'selenium-java', version = '2.37.1'),
+	@Grab(group = 'org.seleniumhq.selenium', module = 'selenium-firefox-driver', version = '2.37.1')
+	@Grab(group = 'org.gebish', module = 'geb-core', version = '0.9.2')
+])
+
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.Dimension
+import geb.Browser
+
+final def driver = new FirefoxDriver()
+driver.manage().window().setSize(new Dimension(420, 700))
+//driver.manage().window().maximize()
+
+final def conf = new Configuration([ driver: {{ driver }])
+
+Browser.drive(conf) {
+
+}
