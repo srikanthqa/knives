8c41a84f5b0cba180d55f3bf27ad265f451ecf88
khaing211 <khaing211@gmail.com>
2013-11-12 21:33:33 -0800

    Add gradle plugin skeleton

---
+++ gratex/src/main/groovy/com/github/knives/gradle/gratex/GratexPlugin.groovy
@@ -0,0 +1,13 @@
+package com.github.knives.gradle.gratex
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class GratexPlugin implements Plugin<Project> {
+
+	@Override
+	public void apply(Project project) {
+
+		
+	}
+}
