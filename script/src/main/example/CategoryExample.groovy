6592152f289ef46970bacf5e3da25b76cbd3be75
khaing211 <khaing211@gmail.com>
2013-11-13 23:13:13 -0800

    Add groovy example of using Category through 'use' keyword

---
+++ script/src/main/example/CategoryExample.groovy
@@ -0,0 +1,59 @@
+/**
+ * Use unobtrusive category in groovy
+ */
+
+class LoopCategory {
+	// retry until max or some condition return true
+	static void until(Range range, Closure closure) {
+		range.any { closure.call(it) }
+	}
+	
+	static void until(List list, Closure closure) {
+		list.any { closure.call(it) }
+	}
+	
+	static List collectWithIndex(List list, Closure closure) {
+		def i = 0
+		return list.collect { closure(it, i++) }
+	}
+	
+	static def findWithIndex(List list, Closure closure) {
+		def i = 0
+		return list.find { closure(it, i++) }
+	}
+	
+	static def findAllWithIndex(List list, Closure closure) {
+		def i = 0
+		return list.findAll { closure(it, i++) }
+	}
+}
+
+use(LoopCategory) {
+	def x = 0
+	(1..4).until {
+		x++
+		return false
+	}
+	
+	assert x == 4
+	
+	def y = 0
+	[1,2,3].until {
+		if (it == 3) return true
+		
+		y++
+		
+		return false
+	}
+	
+	assert y == 2
+	
+	def a = ['a','b','c'].collectWithIndex { obj, i -> return i }
+	assert a == [0,1,2]
+	
+	def b = ['a','b','c'].findWithIndex { obj, i -> return obj == 'b' }
+	assert b == 'b'
+	
+	def c = ['a','b','c'].findAllWithIndex { obj, i -> return i < 2 }
+	assert c == ['a', 'b']
+}
