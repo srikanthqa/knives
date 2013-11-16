524bb87b57b05473b05c142ee49d0c79c5b03703
khaing211 <khaing211@gmail.com>
2013-11-16 09:04:05 -0800

    @Field annotation resolve global variable issue for me :D

---
+++ script/src/main/example/LocalToGlobalVariable.groovy
@@ -0,0 +1,17 @@
+import groovy.transform.Field
+
+@Field def accessible = 'Yay'
+def inaccessiable ='Nah!!!'
+
+def printOut() {
+	println accessible
+	
+	try {
+		println inaccessiable
+	} catch(e) {
+		// should print out MissingPropertyException
+		println e
+	}
+}
+
+printOut()
