d56c66616e6b7d90f554ed9db46f5d99c9371cbf
khaing211 <khaing211@gmail.com>
2013-11-16 09:09:25 -0800

    fine grain locking instead of @Delegate

---
+++ script/src/main/example/AutoLock.groovy
@@ -0,0 +1,21 @@
+import groovy.transform.WithReadLock
+import groovy.transform.WithWriteLock
+
+public class ResourceProvider {
+	private final Map<String, String> data = new HashMap<String, String>();
+
+	@WithReadLock
+	public String getResource(String key) throws Exception {
+		return data.get(key);
+	}
+
+	@WithWriteLock
+	public void putResource(String key, String value = "") throws Exception {
+		data.put(key, value);
+	}
+}
+
+
+ResourceProvider provider = new ResourceProvider()
+provider.putResource("provider")
+println provider.getResource("provider")
