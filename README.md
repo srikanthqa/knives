b56451c19a01313a7b82ee4f20e460c271752ad9
khaing211 <khaing211@gmail.com>
2013-11-17 13:12:31 -0800

    Add custom sourceset project to play around with SourceSet extension gradle

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-knives - who need katana when you have a set of tiny deadly knives
+knives
 ======
 
 A repo contains small and useful scripts across several languages.
-
-Also, this is also my katana repo.
 
 
---
+++ custom-sourceset/src/main/groovy/com/github/knives/gradle/custom/sourceset/CustomSourcesetPlugin.groovy
@@ -0,0 +1,17 @@
+package com.github.knives.gradle.custom.sourceset
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class CustomSourcesetPlugin implements Plugin<Project> {
+
+	public CustomSourcesetPlugin() {
+		
+	}
+	
+	@Override
+	public void apply(final Project project) {
+		// TODO Auto-generated method stub
+		
+	}
+}
