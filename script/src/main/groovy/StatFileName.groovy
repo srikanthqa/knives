93f9d0352767551e03e25d4e697e55fd3cb99705
khaing211 <khaing211@gmail.com>
2013-11-24 20:23:49 -0800

    playing around with FileNameUtil from commons io

---
+++ script/src/main/groovy/StatFileName.groovy
@@ -0,0 +1,32 @@
+@Grapes([
+	@Grab(group='commons-io', module='commons-io', version='2.4')
+])
+			
+import org.apache.commons.io.FilenameUtils
+
+// http://commons.apache.org/proper/commons-io/apidocs/org/apache/commons/io/FilenameUtils.html
+
+final def cli = new CliBuilder(usage: 'GitClone -u <url> -d <path/to/where/to/you/want>')
+cli.h( longOpt: 'help', required: false, 'show usage information' )
+//--------------------------------------------------------------------------
+final def opt = cli.parse(args)
+if (!opt) { return }
+if (opt.h) {
+	cli.usage()
+	return
+}
+
+if (opt.arguments().size() <= 0) {
+	cli.usage()
+}
+
+opt.arguments().each { final filename ->
+	use(FilenameUtils) {
+		println filename.getFullPath()
+		println filename.getFullPathNoEndSeparator()
+		println filename.getName()
+		println filename.getBaseName()
+		println filename.getExtension()
+		println filename.removeExtension()
+	}
+}
