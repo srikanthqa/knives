ad965db224ae629efec68e48cbb1fc7fde92993a
khaing211 <khaing211@gmail.com>
2013-11-24 20:58:50 -0800

    Complete TrimJar example -_-

--- script/src/main/groovy/TrimJar.groovy
+++ script/src/main/groovy/TrimJar.groovy
@@ -5,26 +5,10 @@
  */
             
 @Grapes([
-	@GrabConfig(systemClassLoader=true),
-	@Grab(group='net.sf.proguard', module='proguard-base', version='4.10'),
-	@Grab(group='net.sf.proguard', module='proguard-anttask', version='4.10')
+	@Grab(group='net.sf.proguard', module='proguard-base', version='4.10')
 ]) 
 
-final def cli = new CliBuilder(usage: 'TrimJar <jar> <classpath>')
-cli.h( longOpt: 'help', required: false, 'show usage information' )
-cli.j( longOpt: 'jar', argName: 'j', required: true, args: 1, 'jar files to repack with new class files' )
+import proguard.ProGuard
 
-//--------------------------------------------------------------------------
-final def opt = cli.parse(args)
-if (!opt) { return }
-if (opt.h) {
-	cli.usage();
-	return
-}
-
-final def ant = new AntBuilder()
-
-ant.taskdef(name: 'proguard', classname: 'proguard.ant.ProGuardTask')
-ant.taskdef(name: 'proguardconfiguration', classname: 'proguard.ant.ConfigurationTask')
-
-final def jarFileName = opt.j
+// calling main class of proguard
+ProGuard.main(args)
