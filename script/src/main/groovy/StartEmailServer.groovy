91b040dae176558611228a65502f539eb0c3805e
khaing211 <khaing211@gmail.com>
2013-11-27 00:04:20 -0800

    Add StartEmailServer, support smtp, pop, imap protocol (no secure protocol yet) by using GreenMail jboss package.

---
+++ script/src/main/groovy/StartEmailServer.groovy
@@ -0,0 +1,52 @@
+@Grapes([
+	@Grab(group='com.icegreen', module='greenmail', version='1.3.1b')
+])
+
+import groovy.transform.Canonical
+import com.icegreen.greenmail.util.GreenMail
+import com.icegreen.greenmail.util.ServerSetup
+import javax.mail.internet.MimeMessage
+
+@Canonical
+class RunWhenShuttingDown extends Thread {
+	final GreenMail server
+	
+	public void run() {
+		println "Shutting down the email server"
+		
+		server.stop()
+		
+		final MimeMessage[] messages = server.getReceivedMessages()
+		
+		println "Received ${messages.size()} messages"
+		
+		messages.each {
+			println it
+		}
+	}
+}
+
+final def cli = new CliBuilder(usage: 'StartEmailServer')
+cli.h( longOpt: 'help', required: false, 'show usage information' )
+
+//--------------------------------------------------------------------------
+final def opt = cli.parse(args)
+if (!opt) { return }
+if (opt.h) {
+	cli.usage()
+	return
+}
+
+final def config = [
+	new ServerSetup(8025, "localhost", "smtp"),
+	new ServerSetup(8110, "localhost", "pop3"),
+	new ServerSetup(8143, "localhost", "imap")
+] as ServerSetup[]
+
+final GreenMail server = new GreenMail(config)
+
+Runtime.getRuntime().addShutdownHook(new RunWhenShuttingDown(server))
+
+println "Ctrl+C to stop the email server"
+
+server.start()
