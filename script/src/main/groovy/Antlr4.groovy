c69b8c4cbda6a72dd0cc0052f055733da68a2093
khaing211 <khaing211@gmail.com>
2013-11-28 17:22:09 -0800

    Replace antlr4 commandline $ export CLASSPATH=".:/usr/local/lib/antlr-4.1-complete.jar:$CLASSPATH" $ alias antlr4='java -jar /usr/local/lib/antlr-4.1-complete.jar'

---
+++ script/src/main/groovy/Antlr4.groovy
@@ -0,0 +1,9 @@
+@Grapes([
+	@Grab(group='org.antlr', module='antlr4', version='4.1'),
+	@Grab(group='org.antlr', module='antlr4-runtime', version='4.1')
+])
+			
+
+import org.antlr.v4.Tool
+
+Tool.main(args)
