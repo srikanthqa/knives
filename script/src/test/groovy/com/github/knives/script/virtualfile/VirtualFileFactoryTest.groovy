abc812d9be724ed1531bb1e3788d1ccfc01446d2
khaing211 <khaing211@gmail.com>
2013-12-24 07:20:34 -0800

    Add example test for VirtualFileFactor to be fill out.

---
+++ script/src/test/groovy/com/github/knives/script/virtualfile/VirtualFileFactoryTest.groovy
@@ -0,0 +1,13 @@
+package com.github.knives.script.virtualfile;
+
+import static org.junit.Assert.*
+import org.junit.Test
+
+class VirtualFileFactoryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
