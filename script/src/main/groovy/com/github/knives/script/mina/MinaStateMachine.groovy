c3bef3145415ec9e16c9ff76a66181389b12e383
khaing211 <khaing211@gmail.com>
2013-12-24 07:25:14 -0800

    Fix up Mina State Machine example.

--- script/src/main/groovy/com/github/knives/script/mina/MinaStateMachine.groovy
+++ script/src/main/groovy/com/github/knives/script/mina/MinaStateMachine.groovy
@@ -5,56 +5,51 @@ import org.apache.mina.statemachine.StateMachineProxyBuilder
 import org.apache.mina.statemachine.annotation.State
 import org.apache.mina.statemachine.annotation.Transition
 import org.apache.mina.statemachine.annotation.Transitions
+import org.apache.mina.statemachine.StateMachine
 
 /**
  * http://mina.apache.org/mina-project/userguide/ch14-state-machine/ch14-state-machine.html
  */
 class MinaStateMachine {
-	public interface TapeDeck {
+	public static interface TapeDeck {
 		void load(String nameOfTape);
 		void eject();
 		void start();
 		void pause();
 		void stop();
+		void play();
 	}
 	
-	public class TapeDeckHandler {
-		@State public static final String EMPTY   = "Empty";
-		@State public static final String LOADED  = "Loaded";
-		@State public static final String PLAYING = "Playing";
-		@State public static final String PAUSED  = "Paused";
-	}
-	
-	public class TapeDeckHandler {
+	public static class TapeDeckHandler {
 		@State public static final String EMPTY = "Empty";
 		@State public static final String LOADED = "Loaded";
 		@State public static final String PLAYING = "Playing";
 		@State public static final String PAUSED = "Paused";
 	
-		@Transition(on = "load", in = EMPTY, next = LOADED)
+		@Transition(on = "load", in = TapeDeckHandler.EMPTY, next = TapeDeckHandler.LOADED)
 		public void loadTape(String nameOfTape) {
 			System.out.println("Tape '" + nameOfTape + "' loaded");
 		}
 	
-		@Transitions({
-			@Transition(on = "play", in = LOADED, next = PLAYING),
-			@Transition(on = "play", in = PAUSED, next = PLAYING)
-		})
+		@Transitions([
+			@Transition(on = "play", in = TapeDeckHandler.LOADED, next = TapeDeckHandler.PLAYING),
+			@Transition(on = "play", in = TapeDeckHandler.PAUSED, next = TapeDeckHandler.PLAYING)
+		])
 		public void playTape() {
 			System.out.println("Playing tape");
 		}
 	
-		@Transition(on = "pause", in = PLAYING, next = PAUSED)
+		@Transition(on = "pause", in = TapeDeckHandler.PLAYING, next = TapeDeckHandler.PAUSED)
 		public void pauseTape() {
 			System.out.println("Tape paused");
 		}
 	
-		@Transition(on = "stop", in = PLAYING, next = LOADED)
+		@Transition(on = "stop", in = TapeDeckHandler.PLAYING, next = TapeDeckHandler.LOADED)
 		public void stopTape() {
 			System.out.println("Tape stopped");
 		}
 	
-		@Transition(on = "eject", in = LOADED, next = EMPTY)
+		@Transition(on = "eject", in = TapeDeckHandler.LOADED, next = TapeDeckHandler.EMPTY)
 		public void ejectTape() {
 			System.out.println("Tape ejected");
 		}
