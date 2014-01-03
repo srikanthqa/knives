0eb34c620662f9a4df7b2aae665d3fb5429de8b1
khaing211 <khaing211@gmail.com>
2014-01-03 08:58:58 -0800

    Add JvmTypeConverterTest

---
+++ io/src/test/groovy/com/github/knives/io/JvmTypeConverterTest.groovy
@@ -0,0 +1,83 @@
+package com.github.knives.io;
+
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
+public class JvmTypeConverterTest {
+	@Rule
+	TemporaryFolder folder = new TemporaryFolder();
+	
+	@Before
+	void beforeTest() {
+		
+	}
+	
+	@Test
+	void testStaticField() {
+		// TODO:
+	}
+	
+	@Test
+	void testField() {
+		
+	}
+	
+	@Test
+	void testVoidMethod() {
+		
+	}
+	
+	
+	@Test
+	void testSingleArgMethod() {
+		
+	}
+	
+	@Test
+	void testVaragsMethod() {
+		
+	}
+	
+	@Test
+	void testMultipleArgsMethod() {
+		
+	}
+	
+	@Test
+	void testStaticMethod() {
+		
+	}
+	
+	@Test 
+	void testAnnotation() {
+		
+	}
+	
+	@Test
+	void testTypeAnnotation() {
+		
+	}
+	
+	@Test
+	void testMethodAnnotation() {
+		
+	}
+	
+	@Test
+	void testFieldAnnotation() {
+		
+	}
+	
+	@Test
+	public testArgAnnotation() {
+		
+	}
+	
+	@Test
+	void testEnum() {
+		
+	}
+
+}
