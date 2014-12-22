695e58914ad60f8f0d1320ca5d77f94a4a3bff31
khaing211 <khaing211@gmail.com>
2014-12-21 16:18:34 -0800

    Add commons-compress example project

---
+++ commons-compress-example/src/test/java/com/github/knives/commons/compress/TarGzTest.java
@@ -0,0 +1,14 @@
+package com.github.knives.commons.compress;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TarGzTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
---
+++ commons-compress-example/src/test/java/com/github/knives/commons/compress/ZipTest.java
@@ -0,0 +1,14 @@
+package com.github.knives.commons.compress;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ZipTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
