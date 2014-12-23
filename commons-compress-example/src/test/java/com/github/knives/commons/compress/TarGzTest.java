83f265f0216f908a40004a70ae5c827f61d2173e
khaing211 <khaing211@gmail.com>
2014-12-22 22:03:59 -0800

    Implement write zip/tar.gz using commons-compress

--- commons-compress-example/src/test/java/com/github/knives/commons/compress/TarGzTest.java
+++ commons-compress-example/src/test/java/com/github/knives/commons/compress/TarGzTest.java
@@ -1,14 +1,59 @@
 package com.github.knives.commons.compress;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
 
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
 import org.junit.Test;
 
 public class TarGzTest {
 
 	@Test
-	public void test() {
+	public void testCreate() throws Exception {
+		File file = new File("/tmp/hello.tar.gz");
+		FileOutputStream fileStream = new FileOutputStream(file);
 		
+		GzipCompressorOutputStream gzipOutputStream = new GzipCompressorOutputStream(fileStream);
+		TarArchiveOutputStream tarOutputStream = new TarArchiveOutputStream(gzipOutputStream);
+		TarArchiveEntry tarEntry = new TarArchiveEntry("world.txt");
+		// require
+		tarEntry.setSize(20);
+		
+		tarOutputStream.putArchiveEntry(tarEntry);
+		tarOutputStream.write("compress and archive".getBytes(StandardCharsets.US_ASCII));
+		tarOutputStream.closeArchiveEntry();
+		tarOutputStream.flush();
+		tarOutputStream.close();
+	}
+	
+	@Test
+	public void testCreateDirectory() throws Exception {
+		File file = new File("/tmp/hello1.tar.gz");
+		FileOutputStream fileStream = new FileOutputStream(file);
+		
+		GzipCompressorOutputStream gzipOutputStream = new GzipCompressorOutputStream(fileStream);
+		TarArchiveOutputStream tarOutputStream = new TarArchiveOutputStream(gzipOutputStream);
+		
+		/*
+		// Not required / needed
+		TarArchiveEntry tarDirectoryEntry = new TarArchiveEntry("/hello");
+		tarDirectoryEntry.setMode(TarArchiveEntry.DEFAULT_DIR_MODE);
+		tarOutputStream.putArchiveEntry(tarDirectoryEntry);
+		tarOutputStream.closeArchiveEntry();
+		*/
+		
+		TarArchiveEntry tarEntry = new TarArchiveEntry("/hello/world.txt");
+		tarEntry.setSize(20);
+		
+		tarOutputStream.putArchiveEntry(tarEntry);
+		tarOutputStream.write("compress and archive".getBytes(StandardCharsets.US_ASCII));
+		tarOutputStream.closeArchiveEntry();
+
+		tarOutputStream.flush();
+		tarOutputStream.close();
 	}
 
 }
--- commons-compress-example/src/test/java/com/github/knives/commons/compress/ZipTest.java
+++ commons-compress-example/src/test/java/com/github/knives/commons/compress/ZipTest.java
@@ -1,14 +1,55 @@
 package com.github.knives.commons.compress;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
 
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
+import org.apache.commons.io.FileUtils;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 public class ZipTest {
 
+	@Rule
+	public TemporaryFolder tmpDir = new TemporaryFolder();
+	
 	@Test
-	public void test() {
+	public void testCreateWithFile() throws Exception {
+		File helloTxt = tmpDir.newFile("hello.txt");
+		FileUtils.write(helloTxt, "hello world");
 		
+		ZipArchiveOutputStream zipOutputStream = new ZipArchiveOutputStream(new File("/tmp/hello.zip"));
+		ZipArchiveEntry zipEntry = new ZipArchiveEntry(helloTxt, "world.txt");
+		zipOutputStream.putArchiveEntry(zipEntry);
+		// content of zip is written by write() rather than using constructor of ZipArchiveEntry
+		zipOutputStream.write("hello stupid human".getBytes(StandardCharsets.US_ASCII));
+		zipOutputStream.closeArchiveEntry();
+		zipOutputStream.flush();
+		zipOutputStream.close();
+	}
+	
+	@Test
+	public void testCreate() throws Exception {
+		ZipArchiveOutputStream zipOutputStream = new ZipArchiveOutputStream(new File("/tmp/hello1.zip"));
+		ZipArchiveEntry zipEntry = new ZipArchiveEntry("world1.txt");
+		zipOutputStream.putArchiveEntry(zipEntry);
+		zipOutputStream.write("hello stupid human".getBytes(StandardCharsets.US_ASCII));
+		zipOutputStream.closeArchiveEntry();
+		zipOutputStream.flush();
+		zipOutputStream.close();
+	}
+	
+	@Test
+	public void testCreateDirectory() throws Exception {
+		ZipArchiveOutputStream zipOutputStream = new ZipArchiveOutputStream(new File("/tmp/hello2.zip"));
+		ZipArchiveEntry zipEntry = new ZipArchiveEntry("/hello/world1.txt");
+		zipOutputStream.putArchiveEntry(zipEntry);
+		zipOutputStream.write("hello stupid human".getBytes(StandardCharsets.US_ASCII));
+		zipOutputStream.closeArchiveEntry();
+		zipOutputStream.flush();
+		zipOutputStream.close();
 	}
 
 }
