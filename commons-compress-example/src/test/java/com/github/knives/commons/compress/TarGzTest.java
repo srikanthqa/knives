c1251c0a47202c70bb4f46c92e9e688b7e5131b8
khaing211 <khaing211@gmail.com>
2014-12-22 22:08:48 -0800

    Implement extract .tar.gz

--- commons-compress-example/src/test/java/com/github/knives/commons/compress/TarGzTest.java
+++ commons-compress-example/src/test/java/com/github/knives/commons/compress/TarGzTest.java
@@ -1,12 +1,20 @@
 package com.github.knives.commons.compress;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
+import org.apache.commons.io.FileUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TarGzTest {
@@ -56,4 +64,45 @@ public class TarGzTest {
 		tarOutputStream.close();
 	}
 
+	@Test
+	@Ignore
+	public void testExtract() throws Exception {
+		File file = new File("/tmp/hello.tar.gz");
+		FileInputStream fileStream = new FileInputStream(file);
+		
+		GzipCompressorInputStream gzIn = new GzipCompressorInputStream(fileStream);
+		
+		TarArchiveInputStream archiveInputStream = new TarArchiveInputStream(gzIn);
+		
+		for (TarArchiveEntry archiveEntry = archiveInputStream.getNextTarEntry();
+				archiveEntry != null; 
+				archiveEntry = archiveInputStream.getNextTarEntry()) {
+			
+			System.out.println(archiveEntry.getName());
+			
+			if (archiveEntry.isFile()) {
+				File extractFile = new File(archiveEntry.getName());
+				file.getParentFile().mkdirs();
+				FileUtils.copyInputStreamToFile(new LimitedInputStream(archiveInputStream), extractFile);	
+			} 
+		}
+	}
+	
+	
+	private static class LimitedInputStream extends FilterInputStream {
+		protected LimitedInputStream(InputStream in) {
+			super(in);
+		}
+		
+		// commons-io using this method instead of read(b,offset,length)
+		@Override
+		public int read(byte[] b) throws IOException {
+			return read(b, 0, b.length);
+		}
+		
+		// close() so that commons-io won't close actual stream
+		@Override
+		public void close() { }
+		
+	}
 }
