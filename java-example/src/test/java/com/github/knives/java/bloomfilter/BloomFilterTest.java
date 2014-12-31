8894939f962a7fa0c5eff7ff04b01590940182a2
khaing211 <khaing211@gmail.com>
2014-12-31 08:36:28 -0800

    Add second bloomfilter test

--- java-example/src/test/java/com/github/knives/java/bloomfilter/BloomFilterTest.java
+++ java-example/src/test/java/com/github/knives/java/bloomfilter/BloomFilterTest.java
@@ -6,7 +6,9 @@ import java.security.SecureRandom;
 import org.junit.Test;
 
 import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnel;
 import com.google.common.hash.Funnels;
+import com.google.common.hash.PrimitiveSink;
 
 public class BloomFilterTest {
 
@@ -29,4 +31,32 @@ public class BloomFilterTest {
 		System.out.println(mayBeContained);
 	}
 
+	//Create the custom filter
+	static class BigIntegerFunnel implements Funnel<BigInteger> {
+        @Override
+        public void funnel(BigInteger from, PrimitiveSink into) {
+            into.putBytes(from.toByteArray());
+        }
+    }
+
+	@Test
+	public void test2() {
+		final SecureRandom random = new SecureRandom();
+		BigInteger bigInteger = new BigInteger(8, random);
+		
+		//Creating the BloomFilter
+		BloomFilter<BigInteger> bloomFilter = BloomFilter.create(new BigIntegerFunnel(), 1000);
+
+		//Putting elements into the filter
+		//A BigInteger representing a key of some sort
+		bloomFilter.put(bigInteger);
+
+		//Testing for element in set
+		BigInteger bitIntegerII = new BigInteger(8, random);
+		
+		//Testing for element in set
+		boolean mayBeContained = bloomFilter.mightContain(bitIntegerII);
+		
+		System.out.println(mayBeContained);
+	}
 }
