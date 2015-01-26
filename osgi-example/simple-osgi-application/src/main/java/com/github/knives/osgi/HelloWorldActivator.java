32415f7a9bf86c5c659f4931e6bd829956a72d6b
khaing211 <khaing211@gmail.com>
2015-01-26 15:27:17 -0800

    A simple activator project with external felix testing

---
+++ osgi-example/simple-osgi-application/src/main/java/com/github/knives/osgi/HelloWorldActivator.java
@@ -0,0 +1,19 @@
+package com.github.knives.osgi;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class HelloWorldActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		System.out.println("Hello World");
+
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Goodbye World");
+	}
+
+}
