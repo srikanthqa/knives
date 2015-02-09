b36396fc5fb90d1776b3350ee0949e2c770dbbb1
khaing211 <khaing211@gmail.com>
2015-02-08 21:37:32 -0800

    IO is simply a function, not work until calling run

--- functionaljava-example/src/test/java/com/github/knives/functionaljava/IOFunctionsTest.java
+++ functionaljava-example/src/test/java/com/github/knives/functionaljava/IOFunctionsTest.java
@@ -2,12 +2,15 @@ package com.github.knives.functionaljava;
 
 import org.junit.Test;
 import fj.data.IOFunctions;
+
+import java.io.IOException;
+
 /**
  * Created by developer on 2/8/15.
  */
 public class IOFunctionsTest {
     @Test
-    public void testStdout() {
-        IOFunctions.stdoutPrintln("hello world");
+    public void testStdout() throws IOException {
+        IOFunctions.stdoutPrintln("hello world").run();
     }
 }
