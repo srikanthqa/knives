e7ac10ac4094822cfe5f5b90ec7088bb01dd734e
khaing211 <khaing211@gmail.com>
2015-02-12 06:40:08 -0800

    Setup skeleton activator for whiteboard and eventadmin example remove empty export-package

---
+++ osgi-example/osgi-eventadmin/src/main/java/com/github/knives/osgi/eventadmin/Activator.java
@@ -0,0 +1,20 @@
+package com.github.knives.osgi.eventadmin;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Activator implements BundleActivator {
+    final private static Logger LOG = LoggerFactory.getLogger(Activator.class);
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+
+    }
+}
