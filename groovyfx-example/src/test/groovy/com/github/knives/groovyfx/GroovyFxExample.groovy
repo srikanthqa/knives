6e4b1ca1d8405a673490d0bf42875ea632265629
khaing211 <khaing211@gmail.com>
2015-02-26 06:59:04 -0800

    Add groovyfx example

---
+++ groovyfx-example/src/test/groovy/com/github/knives/groovyfx/GroovyFxExample.groovy
@@ -0,0 +1,26 @@
+package com.github.knives.groovyfx
+
+import static groovyx.javafx.GroovyFX.start
+
+import org.junit.Test
+
+class GroovyFxExample {
+    @Test
+    public void testExample() {
+        start {
+            stage(title: 'GroovyFX Hello World', visible: true) {
+                scene(fill: BLACK, width: 500, height: 250) {
+                    hbox(padding: 60) {
+                        text(text: 'Groovy', font: '80pt sanserif') {
+                            fill linearGradient(endX: 0, stops: [PALEGREEN, SEAGREEN])
+                        }
+                        text(text: 'FX', font: '80pt sanserif') {
+                            fill linearGradient(endX: 0, stops: [CYAN, DODGERBLUE])
+                            effect dropShadow(color: DODGERBLUE, radius: 25, spread: 0.25)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
