6ad8501122426f4de212972b309d258276df4d19
khaing211 <khaing211@gmail.com>
2015-03-23 06:07:24 -0700

    add skeletons code.

---
+++ dojo/src/main/java/com/github/knives/dojo/problem/ArithmeticProgression.java
@@ -0,0 +1,27 @@
+package com.github.knives.dojo.problem;
+
+import java.util.List;
+
+/**
+ * In mathematics, an arithmetic progression (AP) or arithmetic sequence is
+ * a sequence of numbers such that the difference between the consecutive terms is constant.
+ *
+ * For instance, the sequence 5, 7, 9, 11, 13, 15 …
+ * is an arithmetic progression with common difference of 2.
+ */
+public class ArithmeticProgression {
+
+    /**
+     * Given a arithmetic progression with a missing term
+     * Find the missing term
+     *
+     * Constraint:
+     * + sequence.size() >= 3
+     * + term can be positive or negative
+     * + term can be between [-10^6, 10^6]
+     */
+    public static long findMissingTerms(List<Long> sequence) {
+        // TODO
+        return 0;
+    }
+}
