75b3d88bc6decc63c4e2ab7a88bcd4c6306dba13
khaing211 <khaing211@gmail.com>
2015-04-06 20:49:35 -0700

    Graph algorithm, we need two datastructure of graph sometimes.

--- dojo/src/main/java/com/github/knives/dojo/problem/EvenTree.java
+++ dojo/src/main/java/com/github/knives/dojo/problem/EvenTree.java
@@ -1,4 +1,64 @@
 package com.github.knives.dojo.problem;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 public interface EvenTree {
+    public static class Edge {
+        final public int p, q;
+
+        public Edge(int p, int q) {
+            this.p = p;
+            this.q = q;
+        }
+    }
+
+    public static class Node {
+        final public int n;
+        final public List<Node> neighbors = new LinkedList<Node>();
+
+        public Node(int n) {
+            this.n = n;
+        }
+    }
+
+    public static int compute(final Edge[] edges, final Node[] graph) {
+        int removeEdge = 0;
+        for (Edge edge : edges) {
+            Node p = graph[edge.p];
+            Node q = graph[edge.q];
+            if (sizeSubtree(graph.length, p, q) % 2 == 0 && sizeSubtree(graph.length, q, p) % 2 == 0) {
+                p.neighbors.remove(q);
+                q.neighbors.remove(p);
+                removeEdge++;
+            }
+        }
+
+        return removeEdge;
+    }
+
+    public static int sizeSubtree(int n, final Node from, final Node ignore) {
+        final boolean[] visited = new boolean[n];
+        visited[ignore.n] = true;
+        visited[from.n] = true;
+
+        final Stack<Node> nodes = new Stack<>();
+        nodes.push(from);
+
+        int size = 0;
+        while (!nodes.isEmpty()) {
+            Node top = nodes.pop();
+            size++;
+
+            for (Node node : top.neighbors) {
+                if (!visited[node.n]) {
+                    nodes.push(node);
+                    visited[node.n] = true;
+                }
+            }
+        }
+
+        return size;
+    }
 }
--- dojo/src/test/java/com/github/knives/dojo/problem/EvenTreeTest.java
+++ dojo/src/test/java/com/github/knives/dojo/problem/EvenTreeTest.java
@@ -1,4 +1,35 @@
 package com.github.knives.dojo.problem;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class EvenTreeTest {
+    @Test
+    public void test() {
+
+        final EvenTree.Node[] graph = new EvenTree.Node[10];
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new EvenTree.Node(i);
+        }
+
+        final EvenTree.Edge[] edges = new EvenTree.Edge[9];
+        insertEdge(graph, edges, 0, 2, 1);
+        insertEdge(graph, edges, 1, 3, 1);
+        insertEdge(graph, edges, 2, 4, 3);
+        insertEdge(graph, edges, 3, 5, 2);
+        insertEdge(graph, edges, 4, 6, 1);
+        insertEdge(graph, edges, 5, 7, 2);
+        insertEdge(graph, edges, 6, 8, 6);
+        insertEdge(graph, edges, 7, 9, 8);
+        insertEdge(graph, edges, 8, 10, 8);
+
+        assertEquals(2, EvenTree.compute(edges, graph));
+    }
+
+    public static void insertEdge(EvenTree.Node[] graph, EvenTree.Edge[] edges, int i, int q, int p) {
+        graph[q-1].neighbors.add(graph[p-1]);
+        graph[p-1].neighbors.add(graph[q-1]);
+        edges[i] = new EvenTree.Edge(q-1, p-1);
+    }
 }
