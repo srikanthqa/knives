f5ad19fe88ab9a633ac9d48385926d9ef5a7d01c
khaing211 <khaing211@gmail.com>
2015-04-14 20:45:18 -0700

    Add scala value example

---
+++ scala-example/src/main/scala/com/github/knives/scala/Values.scala
@@ -0,0 +1,11 @@
+package com.github.knives.scala
+
+object Values {
+  def main(args: Array[String]) {
+    val whole = 11
+    val fractional = 1.4
+    val words = "A value"
+
+    println(whole, fractional, words)
+  }
+}
