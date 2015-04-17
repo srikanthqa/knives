9bf30139f43ad8852417b7871388032ec8afbd20
khaing211 <khaing211@gmail.com>
2015-04-16 21:13:26 -0700

    Add Variables, Expressions

---
+++ scala-example/src/main/scala/com/github/knives/scala/Expressions.scala
@@ -0,0 +1,16 @@
+package com.github.knives.scala
+
+object Expressions {
+  def main(args: Array[String]) {
+    val c = {
+      val l1 = 2
+      val j1 = 4/l1
+      l1 * j1
+    }
+    
+    println(c)
+    
+    val f = {}
+    println(f)
+  }
+}
---
+++ scala-example/src/main/scala/com/github/knives/scala/Variables.scala
@@ -0,0 +1,11 @@
+package com.github.knives.scala
+
+object Variables {
+  def main(args: Array[String]) {
+    var i:Int = 5
+    println(i)
+    
+    i = 6
+    println(i)
+  }
+}
