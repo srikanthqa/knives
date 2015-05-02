46d5b2b0ef29c5904ac6c5511a0f98b764aa3e83
khaing211 <khaing211@gmail.com>
2015-05-02 14:46:30 -0700

    Add gcd and lcm to MathUtils

--- dojo/src/main/java/com/github/knives/dojo/utils/MathUtils.java
+++ dojo/src/main/java/com/github/knives/dojo/utils/MathUtils.java
@@ -3,9 +3,24 @@ package com.github.knives.dojo.utils;
 import java.util.stream.IntStream;
 
 public interface MathUtils {
-    public static int max(int value1, int ... values) {
+    static int max(int value1, int ... values) {
         return IntStream.concat(IntStream.of(value1), IntStream.of(values))
                 .max()
                 .getAsInt();
+    }
+    
+    static long gcd(long a, long b) {
+        while (a != b) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+
+        return a;
+    }
+
+    static long lcm(long a, long b) {
+        return a / gcd(a,b) * b;
     }
 }
