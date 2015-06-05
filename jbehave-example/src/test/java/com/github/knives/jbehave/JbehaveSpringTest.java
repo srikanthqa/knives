cbae880cfd9efa3d68529ff9670ae6819f18d90a
khaing211 <khaing211@gmail.com>
2015-06-04 23:33:39 -0700

    Configure your own embedder and configuration in jbehave

--- jbehave-example/src/test/java/com/github/knives/jbehave/JbehaveSpringTest.java
+++ jbehave-example/src/test/java/com/github/knives/jbehave/JbehaveSpringTest.java
@@ -1,12 +1,22 @@
 package com.github.knives.jbehave;
 
+import static java.util.Arrays.asList;
+import static org.jbehave.core.io.CodeLocations.codeLocationFromPath;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.embedder.Embedder;
+import org.jbehave.core.io.StoryFinder;
+import org.jbehave.core.steps.spring.SpringStepsFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import javax.inject.Inject;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:/spring/application-context.xml")
@@ -16,9 +26,20 @@ public class JbehaveSpringTest {
 
     @Test
     public void testProgramYourConfiguration() {
-
+		Configuration configuration = new MostUsefulConfiguration();
+    	
+    	Embedder embedder = new Embedder();
+    	embedder.useConfiguration(configuration);
+    	embedder.useStepsFactory(new SpringStepsFactory(configuration, applicationContext));
+    	
+    	embedder.runStoriesAsPaths(storyPaths());
     }
 
+    protected List<String> storyPaths() {
+        String searchInDirectory = codeLocationFromPath("../trader/src/main/java").getFile();
+        return new StoryFinder().findPaths(searchInDirectory, asList("**/*.story"), null);
+    }
+    
     @Inject
     public void setApplicationContext(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
