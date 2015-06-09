c4b960e46d23f007061c96b9d97d976347d36a2f
khaing211 <khaing211@gmail.com>
2015-06-08 18:52:10 -0700

    Add example of observable using iterable

--- rxjava-example/src/test/java/com/github/knives/rxjava/BlockingObservableTest.java
+++ rxjava-example/src/test/java/com/github/knives/rxjava/BlockingObservableTest.java
@@ -3,12 +3,21 @@ package com.github.knives.rxjava;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import rx.Observable;
+import rx.functions.Action1;
 
 public class BlockingObservableTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFromIterable() {
+		Observable.from(new String[] {"clark", "bruce", "diana", "hal"})
+            .subscribe(new Action1<String>() {
+                @Override
+                public void call(String s) {
+                    System.out.println("Hello " + s + "!");
+                }
+
+		    });
 	}
 
 }
