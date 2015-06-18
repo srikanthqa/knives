50f70407c352a2a59d1e38d33dda880a9850d5aa
khaing211 <khaing211@gmail.com>
2015-06-17 22:59:23 -0700

    Add hacky example of how PEG dealing with space

---
+++ parboiled-example/src/main/java/com/github/kn/knives/parboiled/CalculatorParser.java
@@ -0,0 +1,63 @@
+package com.github.kn.knives.parboiled;
+
+import org.parboiled.BaseParser;
+import org.parboiled.Rule;
+import org.parboiled.annotations.BuildParseTree;
+import org.parboiled.support.Var;
+
+@BuildParseTree
+public class CalculatorParser extends BaseParser<Object> {
+    public Rule Expression() {
+        Var<Character> op = new Var<Character>();
+
+        return Sequence(
+                Term(),
+                ZeroOrMore(
+                        Sequence(
+                                FirstOf("+ ", "- "), op.set(matchedChar()),
+                                Term()
+                        )
+                )
+        );
+    }
+ 
+    public Rule Term() {
+        Var<Character> op = new Var<Character>();
+
+        return Sequence(
+                Factor(),
+                ZeroOrMore(
+                        Sequence(
+                                FirstOf("* ", "/ "), op.set(matchedChar()),
+                                Factor()
+                        )
+                )
+        );
+    }
+ 
+    public Rule Factor() {
+        return FirstOf(
+        		Number(),
+                Sequence('(', Expression(), ')')
+        );
+    }
+ 
+    public Rule Number() {
+        return Sequence(OneOrMore(CharRange('0', '9')),  WhiteSpace());
+    }
+    
+    public Rule WhiteSpace() {
+        return ZeroOrMore(AnyOf(" \t\f"));
+    }
+
+    // we redefine the rule creation for string literals to automatically match trailing whitespace if the string
+    // literal ends with a space character, this way we don't have to insert extra whitespace() rules after each
+    // character or string literal
+
+    @Override
+    protected Rule fromStringLiteral(String string) {
+        return string.endsWith(" ") ?
+                Sequence(String(string.substring(0, string.length() - 1)), WhiteSpace()) :
+                String(string);
+    }
+}
