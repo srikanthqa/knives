ae4108fc08186a1e29c26e5924dc4e9df5baf437
khaing211 <khaing211@gmail.com>
2015-06-30 13:50:48 -0700

    Add memory credential store drive to separate refresh_token from the authorization flow.

--- google-api/google-drive-example/src/test/java/com/github/kn/knives/google/drive/MemoryCredentialStoreDrive.java
+++ google-api/google-drive-example/src/test/java/com/github/kn/knives/google/drive/MemoryCredentialStoreDrive.java
@@ -1,19 +1,22 @@
 package com.github.kn.knives.google.drive;
 
 import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.auth.oauth2.StoredCredential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.DataStore;
 import com.google.api.client.util.store.FileDataStoreFactory;
-
-import com.google.api.services.drive.DriveScopes;
-import com.google.api.services.drive.model.*;
+import com.google.api.client.util.store.MemoryDataStoreFactory;
 import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,36 +24,44 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
 
-public class DriveQuickstart {
-    /** Application name. */
+public class MemoryCredentialStoreDrive {
+    /**
+     * Application name.
+     */
     private static final String APPLICATION_NAME =
             "Drive API Java Quickstart";
 
     // arbitrary name to store credential in DataStore
     private static final String USER = "readonly-user";
 
-    /** Directory to store user credentials. */
+    private static final String REFRESH_TOKEN = "your-refresh-token";
+
+    /**
+     * Directory to store user credentials.
+     */
     private static final java.io.File DATA_STORE_DIR = new java.io.File(
             System.getProperty("user.home"), ".credentials/drive-api-quickstart");
 
-    /** Global instance of the {@link FileDataStoreFactory}. */
-    private static FileDataStoreFactory DATA_STORE_FACTORY;
-
-    /** Global instance of the JSON factory. */
+    /**
+     * Global instance of the JSON factory.
+     */
     private static final JsonFactory JSON_FACTORY =
             JacksonFactory.getDefaultInstance();
 
-    /** Global instance of the HTTP transport. */
+    /**
+     * Global instance of the HTTP transport.
+     */
     private static HttpTransport HTTP_TRANSPORT;
 
-    /** Global instance of the scopes required by this quickstart. */
+    /**
+     * Global instance of the scopes required by this quickstart.
+     */
     private static final List<String> SCOPES =
             Arrays.asList(DriveScopes.DRIVE_METADATA_READONLY);
 
     static {
         try {
             HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
         } catch (Throwable t) {
             t.printStackTrace();
             System.exit(1);
@@ -59,6 +70,7 @@ public class DriveQuickstart {
 
     /**
      * Creates an authorized Credential object.
+     *
      * @return an authorized Credential object.
      * @throws IOException
      */
@@ -66,15 +78,18 @@ public class DriveQuickstart {
         // Load client secrets.
         InputStream in =
                 DriveQuickstart.class.getResourceAsStream("/client_secret.json");
-
         GoogleClientSecrets clientSecrets =
                 GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        DataStore<StoredCredential> credentialDataStore = StoredCredential.getDefaultDataStore(MemoryDataStoreFactory.getDefaultInstance());
+        credentialDataStore.set(USER, new StoredCredential().setRefreshToken(REFRESH_TOKEN));
+
 
         // Build flow and trigger user authorization request.
         GoogleAuthorizationCodeFlow flow =
                 new GoogleAuthorizationCodeFlow.Builder(
                         HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                        .setDataStoreFactory(DATA_STORE_FACTORY)
+                        .setCredentialDataStore(credentialDataStore)
                         .setAccessType("offline")
                         .build();
 
@@ -91,6 +106,7 @@ public class DriveQuickstart {
 
     /**
      * Build and return an authorized Drive client service.
+     *
      * @return an authorized Drive client service
      * @throws IOException
      */
@@ -120,5 +136,4 @@ public class DriveQuickstart {
             }
         }
     }
-
 }
